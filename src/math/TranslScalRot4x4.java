29c65c00f5e38e571702d947e374e3b55259c50e
SantiagoGonzalezR <sgonzalez6@eafit.edu.co>
2023-09-15 22:45:07 -0500

    montado el repo

---
+++ src/math/TranslScalRot4x4.java
@@ -0,0 +1,51 @@
+package math;
+
+public class TranslScalRot4x4 extends Matrix4x4 {
+
+    double dx = 0;
+    double dy = 0;
+    double dz = 0;
+    double sx = 1;
+    double sy = 1;
+    double sz = 1;
+    double thetaX = 0;
+    double thetaY = 0;
+    double thetaZ = 0;
+    double centerX = 0;
+    double centerY = 0;
+    double centerZ = 0;
+
+    public static boolean CENTER_TRANSFORMS = false;
+
+    public TranslScalRot4x4() {
+        super();
+    }
+
+    public TranslScalRot4x4(double dx, double dy, double dz,
+            double sx, double sy, double sz,
+            double thetaX, double thetaY, double thetaZ,
+            double centerX, double centerY, double centerZ) {
+        super();
+        
+        // Aplicar transformaciones según la variable CENTER_TRANSFORMS
+        if (CENTER_TRANSFORMS) {
+            // Centro
+            matrix = Matrix4x4.times(this, new Translation4x4(centerX + dx, centerY + dy, centerZ + dz)).matrix;
+        }
+        
+        //Translacion
+        matrix = Matrix4x4.times(this, new Translation4x4(-centerX, -centerY, -centerZ)).matrix;
+        //Escala
+        matrix = Matrix4x4.times(this, new Scaling4x4(sx, sy, sz)).matrix;
+        //Rotaciones
+        matrix = Matrix4x4.times(this, new RotationZ4x4(thetaZ)).matrix;
+        matrix = Matrix4x4.times(this, new RotationY4x4(thetaY)).matrix;
+        matrix = Matrix4x4.times(this, new RotationX4x4(thetaX)).matrix;
+
+
+        if (CENTER_TRANSFORMS) {
+            // Centro
+            matrix = Matrix4x4.times(this, new Translation4x4(centerX + dx, centerY + dy, centerZ + dz)).matrix;
+        }
+    }
+}
